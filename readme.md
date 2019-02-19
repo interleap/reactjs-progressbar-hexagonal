a35f57503308fae6010b955d89753031f1ea4390
omkar6 <omkar@interleap.co>
2019-02-19 14:23:03 +0530

    hexagonal progress bar added

---
+++ readme.md
@@ -0,0 +1,23 @@
+#reactjs-progressabar-linear
+
+[![npm]
+(https://img.shields.io/npm/v/reactjs-progressbar-hexagonal.svg?style=flat)]
+(https://github.com/interleap/reactjs-progressbar-hexagonal)
+
+Based on a pen by Bradley Treweek: https://codepen.io/cmd430/pen/pJKQpg
+
+It diplays the current progress of a task.
+
+## Install
+
+```sh
+npm i -S reactjs-progressbar-hexagonal
+```
+
+## Use
+Put this code wherever required in your react code
+
+```jsx
+<HexagonalProgressBar progress = {25} />
+```
+Pass a prop called progress with the percentage value.
---
+++ src/index.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {withStyles} from '@material-ui/core/styles'
+
+const svgPathLength = 2160;
+
+const styles = () => ({
+    progress : {
+        width: '20vw',
+        height: '40vh',
+        fill: 'rgba(0, 0, 0, 0)',
+        strokeWidth: '30',
+    },
+    track : {
+        stroke: 'rgb(0, 71, 83)',
+        transform: 'translate(290px, 800px)rotate(-120deg)',
+    },
+    fill : {
+        stroke: 'rgb(104, 214, 198)',
+	    strokeLinecap: 'round',
+	    strokeDasharray: `${svgPathLength}`,
+	    strokeDashoffset: `${svgPathLength}`,
+        transition: 'stroke-dashoffset 2s',
+        transform: 'translate(290px, 800px)rotate(-120deg)',
+    },
+    value : {
+        fill : "#fff",
+        fontSize : "14rem"
+    }
+    
+})
+
+
+class HexagonalProgressBar extends Component {
+    setStrokeDashoffset = progress => ((100 - progress) / 100) * svgPathLength
+
+    render() {
+        const {classes} = this.props;
+        return (
+                <div>
+                    <svg className={classes.progress} viewBox="0 0 776 628">
+                    <path className={classes.track} d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
+                    <path className={classes.fill} style={{strokeDashoffset: this.setStrokeDashoffset(this.props.progress) }} d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
+                    <text className={classes.value} x="29%" y="60%">{this.props.progress}%</text>
+                    </svg>
+                </div>
+        );
+    }
+}
+
+export default withStyles(styles)(HexagonalProgressBar);
